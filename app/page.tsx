22aaae1ac310f67a0fb164b2edb466a6c777b102
Yosuke Ushigome <ushi@takram.com>
2023-12-19 23:28:14 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="">
+      <div>
+        I am Yosuke Ushigome, a designer / researcher / technologist based in
+        London.
+      </div>
+    </main>
+  );
+}

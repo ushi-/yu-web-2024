9f7731a799b1104d81169198dae204e5020a9c1d
Yosuke Ushigome <ushi@takram.com>
2024-04-05 12:40:21 +0100

    Temporary commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+"use client";
+
 import Link from "next/link";
-import { compareDesc, format, parseISO } from "date-fns";
+import { useState } from "react";
 import { allPages, Page } from "contentlayer/generated";
+import { ArrowRight, Plus } from "lucide-react";
 
 import { H1 } from "@/components/ui/typography";
 import Header from "@/components/header";
@@ -12,27 +15,44 @@ import {
   CardSecondaryContent,
   CardFooter,
 } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
 
-const PageSection = ({ page }: { page: Page }) => (
-  <Card>
-    <CardContentContainer>
-      <CardPrimaryContentContainer>
-        <CardPrimaryContent>
-          <H1>{page.linkTextsEn[0]}</H1>
-        </CardPrimaryContent>
-        <CardPrimaryContent>
-          <H1>{page.linkTextsJa[0]}</H1>
-        </CardPrimaryContent>
-      </CardPrimaryContentContainer>
-      <CardFooter>
-        <Link href={page.slug} className="lg:text-xl xl:text-2xl">
-          {page.title}
-        </Link>
-      </CardFooter>
-    </CardContentContainer>
-    <CardSecondaryContent />
-  </Card>
-);
+const PageSection = ({ page }: { page: Page }) => {
+  const [linkTextIndex, setLinkTextIndex] = useState(0);
+  const linkTextEn = page.linkTextsEn.slice(0, linkTextIndex + 1).join(" ");
+  const linkTextJa = page.linkTextsJa.slice(0, linkTextIndex + 1).join("");
+  return (
+    <Card>
+      <CardContentContainer>
+        <CardPrimaryContentContainer>
+          <CardPrimaryContent>
+            <H1>{linkTextEn}</H1>
+          </CardPrimaryContent>
+          <CardPrimaryContent>
+            <H1>{linkTextJa}</H1>
+          </CardPrimaryContent>
+        </CardPrimaryContentContainer>
+        <CardFooter>
+          <div className="lg:text-xl xl:text-2xl">
+            {linkTextIndex < page.linkTextsEn.length - 1 ? (
+              <Button
+                variant="ghost"
+                onClick={() => {
+                  setLinkTextIndex((index) => index + 1);
+                }}
+              >
+                {page.title}
+              </Button>
+            ) : (
+              <Link href={page.slug}>{page.title}</Link>
+            )}
+          </div>
+        </CardFooter>
+      </CardContentContainer>
+      <CardSecondaryContent />
+    </Card>
+  );
+};
 
 export default function Home() {
   const pages = allPages.sort((a, b) => a.order - b.order);

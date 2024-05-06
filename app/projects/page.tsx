f74c3b4b17aa3f31dd786024dce78fe5c687df57
Yosuke Ushigome <ushi@takram.com>
2024-05-06 23:13:03 +0100

    Add group hover and link to project summary cards

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 import { compareDesc } from "date-fns";
 import { allProjects, Project } from "contentlayer/generated";
@@ -12,35 +13,49 @@ import {
   CardSecondaryContent,
   CardFooter,
 } from "@/components/ui/card";
-import Image from "next/image";
 
 function ProjectCard(project: Project) {
   return (
-    <Card className="flex-col-reverse gap-2.5">
+    <Card className="flex-col-reverse gap-2.5 group">
       <CardContentContainer className="justify-between">
         <CardPrimaryContentContainer>
           <CardPrimaryContent>
-            <H2>{project.taglineEn}</H2>
+            <Link
+              href={project.url}
+              className="group-hover:text-primary/70 transition-colors"
+            >
+              <H2>{project.taglineEn}</H2>
+            </Link>
           </CardPrimaryContent>
           <CardPrimaryContent>
-            <H2>{project.taglineJa}</H2>
+            <Link
+              href={project.url}
+              className="group-hover:text-primary/70 transition-colors"
+            >
+              <H2>{project.taglineJa}</H2>
+            </Link>
           </CardPrimaryContent>
         </CardPrimaryContentContainer>
         <CardFooter>
-          <Link href={project.url}>{`${project.title}, ${project.year}`}</Link>
+          <Link
+            className="group-hover:text-primary/70 transition-colors"
+            href={project.url}
+          >{`${project.title}, ${project.year}`}</Link>
         </CardFooter>
       </CardContentContainer>
       <CardSecondaryContent>
-        {project.image && (
-          <div className="w-full relative aspect-video">
-            <Image
-              className=" object-cover"
-              src={project.image}
-              alt={project.title}
-              fill
-            />
-          </div>
-        )}
+        <Link href={project.url}>
+          {project.image && (
+            <div className="w-full relative aspect-video overflow-hidden">
+              <Image
+                className=" object-cover group-hover:scale-105 transition-transform"
+                src={project.image}
+                alt={project.title}
+                fill
+              />
+            </div>
+          )}
+        </Link>
       </CardSecondaryContent>
     </Card>
   );
